cf5ac34935ecf75e843dd9beed6e9fc5eff3607d
Dhruv <dhruv162g@gmail.com>
2023-02-27 23:29:53 +0530

    fixed external storage permission

--- app/src/main/java/com/example/spotify_clone/MainActivity.java
+++ app/src/main/java/com/example/spotify_clone/MainActivity.java
@@ -1,20 +1,21 @@
 package com.example.spotify_clone;
 
+import android.Manifest;
+import android.os.Bundle;
+
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
-
-import android.os.Bundle;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     RecyclerView recyclerView;
-    singadapter songadapter;
-    List<song> allsongs=new ArrayList<>();
-    ActivityResultLauncher<String>storagepermissionlauncher;
-    final String permisssion= Manifest.permission.READ_EXTERNAL_STORAGE;
+    com.example.spotify_clone.songAdapter songAdapter;
+    List<song> allSongs=new ArrayList<>();
+    ActivityResultLauncher<String>StoragePermissionLauncher;
+    final String permission = Manifest.permission.READ_EXTERNAL_STORAGE;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
